4d07b70e04758b9e6544b94362490ccce7878ae8
Steve Calvert <scalvert@linkedin.com>
2020-02-14 08:59:37 -0800

    Removing unused util

--- packages/cli/src/utils/get-task-item-totals.ts
+++
@@ -1,9 +0,0 @@
-// import { ITaskItemData } from '../types';
-
-// export default function getTaskItemTotals(data: ITaskItemData): Record<string, number> {
-//   return Object.keys(data).reduce((total: Record<string, number>, type: string) => {
-//     total[type] = data[type].length;
-
-//     return total;
-//   }, {});
-// }
