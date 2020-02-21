9941b3e0f5579852779c083f035f0adea784834e
Lewis Miller <(none)>
2020-02-20 16:34:25 -0800

    feature(plugin-ember-octane): Initial pass at plugin-ember-octane

---
+++ packages/plugin-ember-octane/src/hooks/register-tasks.ts
@@ -0,0 +1,12 @@
+// import { DependenciesTask, ProjectInfoTask, TypesTask } from '../tasks';
+
+import { Hook } from '@oclif/config';
+// import { registerTask } from '@checkup/core';
+
+const hook: Hook<'register-tasks'> = async function() {
+  // registerTask(ProjectInfoTask);
+  // registerTask(DependenciesTask);
+  // registerTask(TypesTask);
+};
+
+export default hook;
