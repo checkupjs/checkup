2fc9a7686e7bf40b3e6aa8ce44dcf7585a7c0ce0
Steve Calvert <scalvert@linkedin.com>
2020-02-21 16:26:41 -0800

    Fixing project info task output

--- packages/plugin-ember/src/tasks/project-info-task.ts
+++ packages/plugin-ember/src/tasks/project-info-task.ts
@@ -9,7 +9,7 @@ export default class ProjectInfoTask extends BaseTask {
     let result: ProjectInfoTaskResult = new ProjectInfoTaskResult();
     let pkg: PackageJson = getPackageJson(this.args.path);
 
-    result.type = getProjectType(this.args.path);
+    result.type = `Ember.js ${getProjectType(this.args.path)}`;
     result.name = pkg.name || '';
     result.version = pkg.version || '';
 
