f4a5d48d72e74c4b1a66a930685655b09a771e3e
Steve Calvert <scalvert@linkedin.com>
2020-02-22 15:36:56 -0800

    Removing unused dependency

--- packages/core/src/base-task.ts
+++ packages/core/src/base-task.ts
@@ -1,7 +1,5 @@
 import { Task, TaskResult } from './types';
 
-// import { titleCase } from 'title-case';
-
 export default abstract class BaseTask implements Task {
   static taskName: string;
   static friendlyTaskName: string;
