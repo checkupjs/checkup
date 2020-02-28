5e1b04be250db34aec50bd96fa1dfd72a0f24ac8
Steve Calvert <scalvert@linkedin.com>
2020-02-27 16:27:51 -0800

    Fixing tests

--- packages/parser-eslint/src/hooks/register-parsers.ts
+++ packages/parser-eslint/src/hooks/register-parsers.ts
@@ -2,7 +2,6 @@ import ESLintParser from '../eslint-parser';
 import { Hook } from '@oclif/config';
 
 const hook: Hook<'register-parsers'> = async function({ registerParser }: any) {
-  process.stdout.write('in here registering parsers');
   registerParser('eslint', new ESLintParser());
 };
 
