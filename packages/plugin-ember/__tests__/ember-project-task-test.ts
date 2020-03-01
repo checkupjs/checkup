ae8cc90ab2a663a008bce1eab83bfa13f81dd60e
Steve Calvert <scalvert@linkedin.com>
2020-02-29 18:59:46 -0800

    Cleanup implementations of fixturify-project classes (#56)

--- packages/plugin-ember/__tests__/ember-project-task-test.ts
+++ packages/plugin-ember/__tests__/ember-project-task-test.ts
@@ -1,29 +1,29 @@
-import { Project, stdout } from '@checkup/test-helpers';
+import { EmberProject, stdout } from '@checkup/test-helpers';
 import { EmberProjectTask } from '../src/tasks';
 import { EmberProjectTaskResult } from '../src/results';
 
 describe('project-info-task', () => {
-  let fixturifyProject: Project;
+  let project: EmberProject;
 
   describe('for Ember Applications', () => {
     beforeEach(() => {
       let packageJson = require('./__fixtures__/app-package.json');
 
-      fixturifyProject = new Project('checkup-app', '0.0.0', project => {
+      project = new EmberProject('checkup-app', '0.0.0', project => {
         project.addDependency('ember-cli', '^3.15.0');
       });
 
-      fixturifyProject.updatePackageJson(packageJson);
+      project.updatePackageJson(packageJson);
 
-      fixturifyProject.writeSync();
+      project.writeSync();
     });
 
     afterEach(() => {
-      fixturifyProject.dispose();
+      project.dispose();
     });
 
     it('can read project info and output to console', async () => {
-      const result = await new EmberProjectTask({ path: fixturifyProject.baseDir }).run();
+      const result = await new EmberProjectTask({ path: project.baseDir }).run();
       const taskResult = <EmberProjectTaskResult>result;
 
       taskResult.toConsole();
@@ -32,7 +32,7 @@ describe('project-info-task', () => {
     });
 
     it('can read project info as JSON', async () => {
-      const result = await new EmberProjectTask({ path: fixturifyProject.baseDir }).run();
+      const result = await new EmberProjectTask({ path: project.baseDir }).run();
       const taskResult = <EmberProjectTaskResult>result;
 
       expect(taskResult.toJson()).toMatchSnapshot();
@@ -43,21 +43,21 @@ describe('project-info-task', () => {
     beforeEach(() => {
       let packageJson = require('./__fixtures__/addon-package.json');
 
-      fixturifyProject = new Project('checkup-app', '0.0.0', project => {
+      project = new EmberProject('checkup-app', '0.0.0', project => {
         project.addDependency('ember-cli', '^3.15.0');
       });
 
-      fixturifyProject.updatePackageJson(packageJson);
+      project.updatePackageJson(packageJson);
 
-      fixturifyProject.writeSync();
+      project.writeSync();
     });
 
     afterEach(() => {
-      fixturifyProject.dispose();
+      project.dispose();
     });
 
     it('can read project info and output to console', async () => {
-      const result = await new EmberProjectTask({ path: fixturifyProject.baseDir }).run();
+      const result = await new EmberProjectTask({ path: project.baseDir }).run();
       const taskResult = <EmberProjectTaskResult>result;
 
       taskResult.toConsole();
@@ -66,7 +66,7 @@ describe('project-info-task', () => {
     });
 
     it('can read project info as JSON', async () => {
-      const result = await new EmberProjectTask({ path: fixturifyProject.baseDir }).run();
+      const result = await new EmberProjectTask({ path: project.baseDir }).run();
       const taskResult = <EmberProjectTaskResult>result;
 
       expect(taskResult.toJson()).toMatchSnapshot();
--- packages/test-helpers/src/index.ts
+++ packages/test-helpers/src/index.ts
@@ -1,4 +1,4 @@
-export { default as Project } from './ember-cli-fixturify-project';
 export { stdout } from './stdout';
 export { default as CheckupProject } from './checkup-fixturify-project';
+export { default as EmberProject } from './ember-cli-fixturify-project';
 export { default as Plugin } from './plugin';
