623dfe4d3758afc06728d44c50b7d6b276268429
Lewis Miller <(none)>
2020-03-05 14:58:02 -0800

    feat(plugin-ember-octane): output completion info

--- packages/plugin-ember-octane/__tests__/results/octane-migration-status-tast-result-test.ts
+++ packages/plugin-ember-octane/__tests__/results/octane-migration-status-tast-result-test.ts
@@ -1,6 +1,7 @@
 import { stdout } from '@checkup/test-helpers';
 // import { OctaneMigrationStatusTask } from '../../src/tasks';
 import { OctaneMigrationStatusTaskResult } from '../../src/results';
+import { MigrationType } from '../../src/results/octane-migration-status-task-result';
 
 // classic-decorator-hooks                  -
 // classic-decorator-no-classic-methods     -
@@ -32,8 +33,26 @@ describe('octane-migration-status-task-result', () => {
       let taskResult = new OctaneMigrationStatusTaskResult(sampleOctaneReport);
 
       let jsonResults = taskResult.toJson();
+      let { totalViolations, migrationTasks } = jsonResults;
+      let nativeClassesMigrationInfo = migrationTasks[MigrationType.NativeClasses];
 
-      expect(jsonResults).toBeDefined();
+      expect(totalViolations).toBe(20);
+      expect(nativeClassesMigrationInfo).toBeDefined();
+      expect(nativeClassesMigrationInfo.name).toBe('Native Class Migration');
+    });
+
+    test('it should output detailed completion data', () => {
+      let sampleOctaneReport = require('../__fixtures__/sample-octane-eslint-report.json');
+      let taskResult = new OctaneMigrationStatusTaskResult(sampleOctaneReport);
+
+      let jsonResults = taskResult.toJson();
+      let { migrationTasks } = jsonResults;
+      let nativeClassesMigrationInfo = migrationTasks[MigrationType.NativeClasses];
+
+      expect(nativeClassesMigrationInfo).toBeDefined();
+      expect(nativeClassesMigrationInfo.completionInfo.total).toBe(6);
+      expect(nativeClassesMigrationInfo.completionInfo.completed).toBe(1);
+      expect(nativeClassesMigrationInfo.completionInfo.percentage).toBe('16.67');
     });
   });
 });
