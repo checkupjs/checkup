b79aba2f255898cae1e02405dc032014bce435f8
Lewis Miller <(none)>
2020-03-06 10:23:20 -0800

    cleanup comments

--- packages/plugin-ember-octane/__tests__/results/octane-migration-status-tast-result-test.ts
+++ packages/plugin-ember-octane/__tests__/results/octane-migration-status-tast-result-test.ts
@@ -1,19 +1,6 @@
 import { stdout } from '@checkup/test-helpers';
-// import { OctaneMigrationStatusTask } from '../../src/tasks';
 import { OctaneMigrationStatusTaskResult } from '../../src/results';
 import { MigrationType } from '../../src/results/octane-migration-status-task-result';
-
-// classic-decorator-hooks                  -
-// classic-decorator-no-classic-methods     -
-// no-actions-hash                          - 3
-// no-classic-classes                       - 5
-// no-classic-components                    - 5
-// no-component-lifecycle-hooks             -
-// no-computed-properties-in-native-classes -
-// no-get-with-default                      -
-// no-get                                   - 3
-// no-jquery                                -
-// require-tagless-components               - 4
 
 describe('octane-migration-status-task-result', () => {
   describe('console output', () => {
