715b062b08fba509b9ec7f8e4be034cec8b2b781
Steve Calvert <scalvert@linkedin.com>
2020-03-23 14:21:26 -0700

    Renaming test-dir to test-root

--- packages/test-helpers/src/index.ts
+++ packages/test-helpers/src/index.ts
@@ -1,6 +1,6 @@
 export { stdout } from './stdout';
 export { createTmpDir } from './tmp-dir';
-export { testRoot } from './test-dir';
+export { testRoot } from './test-root';
 export { default as CheckupProject } from './checkup-fixturify-project';
 export { default as CheckupPluginProject } from './checkup-plugin-project';
 export { default as EmberProject } from './ember-cli-fixturify-project';
