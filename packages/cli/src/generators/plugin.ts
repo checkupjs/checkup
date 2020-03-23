75d53ec2ec194807b47e1b11e9a7a331a5a853ad
Steve Calvert <scalvert@linkedin.com>
2020-03-22 17:21:24 -0700

    task(generators): Adding plugin generator

---
+++ packages/cli/src/generators/plugin.ts
@@ -0,0 +1,115 @@
+import * as Generator from 'yeoman-generator';
+import * as chalk from 'chalk';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const { version } = require('../../package.json');
+
+export default class PluginGenerator extends Generator {
+  answers!: {
+    typescript: boolean;
+    description: string;
+    author: string;
+    repository: string;
+  };
+
+  private get _ext() {
+    return this.options.typescript ? 'ts' : 'js';
+  }
+
+  async prompting() {
+    this.log(
+      `Adding ${chalk.bold.white(this.options.name)} plugin. Version: ${chalk.bold.white(version)}`
+    );
+
+    const defaults = {
+      typescript: true,
+      description: 'Checkup plugin',
+      author: '',
+      repository: '',
+    };
+
+    if (this.options.defaults) {
+      this.answers = defaults;
+    } else {
+      this.answers = await this.prompt([
+        {
+          type: 'confirm',
+          name: 'typescript',
+          message: 'TypeScript',
+          default: () => true,
+        },
+        {
+          type: 'input',
+          name: 'description',
+          message: 'Description',
+          default: 'Checkup plugin',
+        },
+        {
+          type: 'input',
+          name: 'author',
+          message: 'Author',
+          default: '',
+        },
+        {
+          type: 'input',
+          name: 'repository',
+          message: 'Repository',
+          default: '',
+        },
+      ]);
+    }
+
+    this.options.typescript = this.answers.typescript;
+    this.options.description = this.answers.description;
+    this.options.author = this.answers.author;
+    this.options.repository = this.answers.repository;
+  }
+
+  writing() {
+    this.sourceRoot(path.join(__dirname, '../../templates/src/plugin'));
+    this.destinationRoot(path.join(process.cwd(), this.options.name));
+
+    this.fs.copyTpl(
+      this.templatePath(`src/index.${this._ext}.ejs`),
+      this.destinationPath(`src/index.${this._ext}`),
+      this.options
+    );
+
+    this.fs.copyTpl(
+      this.templatePath(`src/hooks/register-tasks.${this._ext}.ejs`),
+      this.destinationPath(`src/hooks/register-tasks.${this._ext}`),
+      this.options
+    );
+
+    this.fs.copyTpl(
+      this.templatePath(`src/tasks/index.${this._ext}.ejs`),
+      this.destinationPath(`src/tasks/index.${this._ext}`),
+      this.options
+    );
+
+    this.fs.copyTpl(
+      this.templatePath(`src/types/index.${this._ext}.ejs`),
+      this.destinationPath(`src/types/index.${this._ext}`),
+      this.options
+    );
+
+    this.fs.copy(this.templatePath('jest.config.js'), this.destinationPath('jest.config.js'));
+    this.fs.copy(this.templatePath('tsconfig.json'), this.destinationPath('tsconfig.json'));
+
+    this.fs.copyTpl(
+      this.templatePath('package.json.ejs'),
+      this.destinationPath('package.json'),
+      this.options
+    );
+
+    this.fs.copyTpl(
+      this.templatePath('README.md.ejs'),
+      this.destinationPath('README.md'),
+      this.options
+    );
+
+    fs.mkdirSync(this.destinationPath('__tests__'), { recursive: true });
+    fs.mkdirSync(this.destinationPath('src/results'), { recursive: true });
+  }
+}
