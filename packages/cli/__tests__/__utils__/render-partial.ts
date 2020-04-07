7724bd2522092eeade6523e7f2bb34d1a5ec554c
Cara Kessler <carakessler@gmail.com>
2020-04-07 16:46:43 -0700

    Implementing numerical-card
    Implemented numerical-card and wrote tests. Added in support to render partials for tests as well, which will be used by other partials

---
+++ packages/cli/__tests__/__utils__/render-partial.ts
@@ -0,0 +1,34 @@
+import { ReportResultData, ReportComponentType } from '@checkup/core';
+import { readFileSync } from 'fs-extra';
+import * as Handlebars from 'handlebars';
+import * as path from 'path';
+
+export type CompiledPartials = {
+  [key in ReportComponentType]: HandlebarsTemplateDelegate;
+};
+
+const compiledPartials: CompiledPartials = {
+  [ReportComponentType.NumericalCard]: getPartialDelegate('numerical-card.hbs'),
+  [ReportComponentType.Table]: getPartialDelegate('table.hbs'),
+  [ReportComponentType.GradedTable]: getPartialDelegate('graded-table.hbs'),
+  [ReportComponentType.PieChart]: getPartialDelegate('pie-chart.hbs'),
+};
+
+/*
+ * When testing partials rendering multiple times in the same module,
+ * it is efficient to compile them once in module scope and pass the precompiled
+ * partials to the `renderPartialAsHtml` function
+ **/
+function getPartialDelegate(partialPath: string): HandlebarsTemplateDelegate {
+  let fullPartialPath = path.join(__dirname, `../../src/static/components/${partialPath}`);
+  let partialRaw = readFileSync(fullPartialPath, 'utf8');
+  return Handlebars.compile(partialRaw);
+}
+
+export function renderPartialAsHtml(componentData: ReportResultData): string {
+  // TODO: remove this check once all tasks are retrofitted to return results (and undefined  is no longer a valid option for ReportResultsData)
+  if (componentData) {
+    return compiledPartials[componentData.componentType]({ taskResult: componentData });
+  }
+  return '';
+}
