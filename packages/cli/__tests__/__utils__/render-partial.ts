53a96c17f3c0a24613932eb62f634c9424a9350e
carakessler <carakessler@gmail.com>
2020-04-10 12:02:09 -0700

    Implementing numerical-card (#222)
    * Implementing numerical-card
    
    Implemented numerical-card and wrote tests. Added in support to render partials for tests as well, which will be used by other partials
    
    * Update packages/cli/__tests__/__utils__/render-partial.ts
    
    Co-Authored-By: Steve Calvert <scalvert@linkedin.com>
    
    * Update packages/cli/__tests__/__utils__/render-partial.ts
    
    Co-Authored-By: Steve Calvert <scalvert@linkedin.com>
    
    * Implementing numerical-card
    
    Implemented numerical-card and wrote tests. Added in support to render partials for tests as well, which will be used by other partials
    
    * Update render-partial.ts
    
    * Update render-partial.ts
    
    Co-authored-by: Steve Calvert <scalvert@linkedin.com>

---
+++ packages/cli/__tests__/__utils__/render-partial.ts
@@ -0,0 +1,30 @@
+import { ReportResultData, ReportComponentType } from '@checkup/core';
+import { readFileSync } from 'fs-extra';
+import * as Handlebars from 'handlebars';
+import * as path from 'path';
+
+export type CompiledPartials = {
+  [key in ReportComponentType]: HandlebarsTemplateDelegate;
+};
+
+const COMPILED_PARTIALS: CompiledPartials = {
+  [ReportComponentType.NumericalCard]: getPartialDelegate('numerical-card.hbs'),
+  [ReportComponentType.Table]: getPartialDelegate('table.hbs'),
+  [ReportComponentType.GradedTable]: getPartialDelegate('graded-table.hbs'),
+  [ReportComponentType.PieChart]: getPartialDelegate('pie-chart.hbs'),
+};
+
+function getPartialDelegate(partialPath: string): HandlebarsTemplateDelegate {
+  let fullPartialPath = path.join(__dirname, `../../src/static/components/${partialPath}`);
+  let partialRaw = readFileSync(fullPartialPath, 'utf8');
+  return Handlebars.compile(partialRaw);
+}
+
+export function renderPartialAsHtml(componentData: ReportResultData): string {
+  // TODO: remove this check once all tasks are retrofitted to return results (and undefined  is no longer a valid option for ReportResultsData)
+  if (componentData) {
+    return COMPILED_PARTIALS[componentData.componentType]({ taskResult: componentData });
+  }
+
+  return '';
+}
