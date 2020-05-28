02ebd00293075508bf0ea94c94c94b6cd2d1baa4
Steve Calvert <steve.calvert@gmail.com>
2020-05-28 16:28:51 -0700

    Deleting unused code related to deprecated PDF/HTML functionality (#449)

--- packages/core/src/report-components/report-component-data.ts
+++
@@ -1,5 +0,0 @@
-import { ReportComponentType, TaskMetaData } from '../types/tasks';
-
-export abstract class ReportComponentData {
-  constructor(public meta: TaskMetaData, public componentType: ReportComponentType) {}
-}
