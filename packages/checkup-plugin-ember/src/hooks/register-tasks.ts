09ae61b8aeeb478c310761527e26266c5faa751f
carakessler <carakessler@gmail.com>
2020-06-04 10:14:50 -0700

    Adding task that checks how many times ember template-lint is disabled (#469)

--- packages/checkup-plugin-ember/src/hooks/register-tasks.ts
+++ packages/checkup-plugin-ember/src/hooks/register-tasks.ts
@@ -4,6 +4,7 @@ import EmberTestTypesTaskTask from '../tasks/ember-test-types-task';
 import EmberTypesTask from '../tasks/ember-types-task';
 import { Hook } from '@oclif/config';
 import { getPluginName } from '@checkup/core';
+import TemplateLintDisableTask from '../tasks/template-lint-disable-task';
 
 const hook: Hook<'register-tasks'> = async function ({ context, tasks }: any) {
   let pluginName = getPluginName(__dirname);
@@ -12,6 +13,7 @@ const hook: Hook<'register-tasks'> = async function ({ context, tasks }: any) {
   tasks.registerTask(new EmberDependenciesTask(pluginName, context));
   tasks.registerTask(new EmberInRepoAddonsEnginesTask(pluginName, context));
   tasks.registerTask(new EmberTestTypesTaskTask(pluginName, context));
+  tasks.registerTask(new TemplateLintDisableTask(pluginName, context));
 };
 
 export default hook;
