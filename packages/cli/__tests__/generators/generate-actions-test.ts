3c2914db40b75d5671d5132b924c0c6a8179654d
Steve Calvert <scalvert@linkedin.com>
2020-08-10 10:12:59 -0700

    feat: Adding actions generator

---
+++ packages/cli/__tests__/generators/generate-actions-test.ts
@@ -0,0 +1,106 @@
+/* eslint-disable jest/expect-expect */
+
+import * as helpers from 'yeoman-test';
+import { resolve } from 'path';
+
+import ActionsGenerator from '../../src/generators/actions';
+import { generatePlugin } from '../__utils__/generate-plugin';
+import { testRoot } from '@checkup/test-helpers';
+
+function assertActionsFiles(name: string, dir: string, extension: string = 'ts') {
+  let root = testRoot(dir);
+
+  expect(root.file(`src/actions/${name}-actions.${extension}`).contents).toMatchSnapshot();
+}
+
+function assertPluginFiles(dir: string, extension: string = 'ts') {
+  let root = testRoot(dir);
+
+  expect(root.file(`src/hooks/register-tasks.${extension}`).contents).toMatchSnapshot();
+}
+
+describe('actions generator', () => {
+  it('generates correct files with TypeScript', async () => {
+    let baseDir = await generatePlugin();
+    let dir = await helpers
+      .run(ActionsGenerator, { namespace: 'checkup:actions' })
+      .cd(baseDir)
+      .withOptions({
+        name: 'my-foo',
+      })
+      .withPrompts({
+        taskName: 'foo',
+        typescript: true,
+      });
+
+    assertActionsFiles('my-foo', dir);
+    assertPluginFiles(dir);
+  });
+
+  it('generates correct files with TypeScript in custom path', async () => {
+    let baseDir = await generatePlugin({ path: './lib' });
+    await helpers
+      .run(ActionsGenerator, { namespace: 'checkup:actions' })
+      .cd(resolve(baseDir, '../..'))
+      .withOptions({
+        name: 'my-foo',
+        path: './lib/checkup-plugin-my-plugin',
+      })
+      .withPrompts({
+        taskName: 'foo',
+        typescript: true,
+      });
+
+    assertActionsFiles('my-foo', baseDir);
+    assertPluginFiles(baseDir);
+  });
+
+  it('generates multiple correct files with TypeScript', async () => {
+    let baseDir = await generatePlugin();
+    let dir = await helpers
+      .run(ActionsGenerator, { namespace: 'checkup:actions' })
+      .cd(baseDir)
+      .withOptions({
+        name: 'my-foo',
+      })
+      .withPrompts({
+        taskName: 'foo',
+        typescript: true,
+      });
+
+    dir = await helpers
+      .run(ActionsGenerator, { namespace: 'checkup:actions' })
+      .cd(baseDir)
+      .withOptions({
+        name: 'my-bar',
+      })
+      .withPrompts({
+        taskName: 'foo',
+        typescript: true,
+      });
+
+    assertActionsFiles('my-foo', dir);
+    assertActionsFiles('my-bar', dir);
+    assertPluginFiles(dir);
+  });
+
+  it('generates correct files with JavaScript', async () => {
+    let baseDir = await generatePlugin(
+      { name: 'my-plugin', defaults: false },
+      { typescript: false }
+    );
+    let dir = await helpers
+      .run(ActionsGenerator, { namespace: 'checkup:actions' })
+      .cd(baseDir)
+      .withOptions({
+        name: 'my-foo',
+      })
+      .withPrompts({
+        taskName: 'foo',
+        typescript: false,
+      });
+
+    assertActionsFiles('my-foo', dir, 'js');
+    assertPluginFiles(dir, 'js');
+  });
+});
