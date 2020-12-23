02abec3bbf388fcd6b37afdb5ebcfd9f5306ef64
Cara Kessler <carakessler@gmail.com>
2020-12-23 12:25:12 -0800

    Removing meta-task as a concept, moving logic from project-meta-task into get-log
    There is a lot of infrastructure around meta-tasks that is not really being used. Given that there is consensus that it doesnt seem likely for there to be any additional metatasks, removed the infrastructure and moved the logic of the task into get-log.

--- packages/cli/src/base-meta-task-result.ts
+++
@@ -1,9 +0,0 @@
-import { TaskIdentifier } from '@checkup/core';
-
-export default abstract class BaseMetaTaskResult {
-  meta: TaskIdentifier;
-
-  constructor(meta: TaskIdentifier) {
-    this.meta = meta;
-  }
-}
