4ba5e0ef0db73125b8a6228ae684fb32a6d727ae
carakessler <carakessler@gmail.com>
2021-01-06 13:42:12 -0800

    Removing meta-task as a concept, moving logic from project-meta-task into get-log (#830)
    There is a lot of infrastructure around meta-tasks that is not really being used. Given that there is consensus that it doesnt seem likely for there to be any additional metatasks, removed the infrastructure and moved the logic of the task into get-log.

--- packages/cli/src/get-log.ts
+++ packages/cli/src/get-log.ts
@@ -1,22 +1,32 @@
-import { Action, CheckupMetadata, Task } from '@checkup/core';
-import { Invocation, Log, Result, ReportingDescriptor } from 'sarif';
+import {
+  Action,
+  CheckupConfig,
+  CheckupMetadata,
+  normalizePaths,
+  Task,
+  TaskContext,
+} from '@checkup/core';
+import * as crypto from 'crypto';
+import * as stringify from 'json-stable-stringify';
+import { Invocation, Log, ReportingDescriptor, Result } from 'sarif';
+import { getVersion } from './utils/get-version';
+import { getRepositoryInfo } from './utils/repository';
 import TaskList from './task-list';
-import { MetaTaskResult } from './types';
 
-export function getLog(
-  info: MetaTaskResult[],
+export async function getLog(
+  taskContext: TaskContext,
   taskResults: Result[],
   actions: Action[],
   invocation: Invocation,
   taskList: TaskList,
   executedTasks: Task[]
-): Log {
-  let _info = Object.assign({}, ...info.map((result) => result.getData())) as CheckupMetadata;
+): Promise<Log> {
+  let checkupMetadata = await getCheckupMetadata(taskContext);
 
   return {
     version: '2.1.0',
     $schema: 'https://schemastore.azurewebsites.net/schemas/json/sarif-2.1.0-rtm.5.json',
-    properties: { ..._info, actions, timings: taskList.timings },
+    properties: { ...checkupMetadata, actions, timings: taskList.timings },
     runs: [
       {
         results: taskResults,
@@ -27,7 +37,7 @@ export function getLog(
             rules: getReportingDescriptors(executedTasks),
             language: 'en-US',
             informationUri: 'https://github.com/checkupjs/checkup',
-            version: _info.cli.version,
+            version: checkupMetadata.cli.version,
           },
         },
       },
@@ -48,4 +58,46 @@ function getReportingDescriptors(tasks: Task[]): ReportingDescriptor[] {
       properties: { enabled: task.enabled, group: task.group, category: task.category },
     };
   });
+}
+
+function getConfigHash(checkupConfig: CheckupConfig) {
+  let configAsJson = stringify(checkupConfig);
+
+  return crypto.createHash('md5').update(configAsJson).digest('hex');
+}
+
+async function getCheckupMetadata(taskContext: TaskContext): Promise<CheckupMetadata> {
+  let package_ = taskContext.pkg;
+  let repositoryInfo = await getRepositoryInfo(taskContext.cliFlags.cwd, taskContext.paths);
+
+  let { config, task, format } = taskContext.cliFlags;
+  let analyzedFiles = normalizePaths(taskContext.paths, taskContext.cliFlags.cwd);
+
+  return {
+    project: {
+      name: package_.name || '',
+      version: package_.version || '',
+      repository: repositoryInfo,
+    },
+
+    cli: {
+      configHash: getConfigHash(taskContext.config),
+      config: taskContext.config,
+      version: getVersion(),
+      schema: 1,
+      args: {
+        paths: taskContext.cliArguments,
+      },
+      flags: {
+        config,
+        task,
+        format,
+        outputFile: taskContext.cliFlags['output-file'],
+        excludePaths: taskContext.cliFlags['exclude-paths'],
+      },
+    },
+
+    analyzedFiles,
+    analyzedFilesCount: analyzedFiles.length,
+  };
 }
