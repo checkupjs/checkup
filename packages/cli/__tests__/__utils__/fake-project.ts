c0b00c2b4b572fdd8f2fbfc179a88a39161c6d1a
Steve Calvert <steve.calvert@gmail.com>
2021-03-30 17:03:21 -0700

    Update packages/cli/__tests__/__utils__/fake-project.ts
    Co-authored-by: Robert Jackson <me@rwjblue.com>

--- packages/cli/__tests__/__utils__/fake-project.ts
+++ packages/cli/__tests__/__utils__/fake-project.ts
@@ -10,13 +10,14 @@ import { mkdirp, symlink } from 'fs-extra';
 export class FakeProject extends CheckupProject {
   async addPlugin(options: helpers.Dictionary<any> = {}, prompts: Answers = {}) {
     let pluginDir = await generatePlugin(options, prompts, join(this.baseDir, 'node_modules'));
-    let coreDir = join(pluginDir, 'node_modules', '@checkup', 'core');
+    let source = join(__dirname, '../../..', 'core');
+    let target = join(pluginDir, 'node_modules', '@checkup', 'core');
 
     // we create a self-referential link to the core package within the generated plugin. This
     // allows us to generate plugins, and test the APIs for the latest version of core that is
     // referenced via the plugins' node_modules.
-    await mkdirp(dirname(coreDir));
-    await symlink(join(__dirname, '../../..', 'core'), coreDir);
+    await mkdirp(dirname(target));
+    await symlink(source, target);
 
     return pluginDir;
   }
