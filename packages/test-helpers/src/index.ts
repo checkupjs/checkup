5ede45309fe67189fec9a8fa680c52c1c7e54dca
Steve Calvert <steve.calvert@gmail.com>
2021-04-07 13:09:43 -0700

    chore: Removes unused APIs (#924)

--- packages/test-helpers/src/index.ts
+++ packages/test-helpers/src/index.ts
@@ -1,6 +1,5 @@
 export { createTmpDir } from './tmp-dir';
 export { testRoot } from './test-root';
 export { getTaskContext } from './get-task-context';
-export { stableJson } from './filter-result-data';
 export { default as CheckupProject } from './checkup-fixturify-project';
 export { default as EmberProject } from './ember-cli-fixturify-project';
