4228b987cca4416fb7849983c0e387d4144f6dd0
Zhan Wang <zhanwang@zhanwang-mn1.linkedin.biz>
2021-07-29 19:01:34 -0700

    project init

---
+++ packages/checkup-formatter-pretty/jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  transform: { '\\.(ts|tsx)$': 'ts-jest' },
+  coverageReporters: ['lcov', 'text-summary'],
+  // collectCoverage: !!process.env.CI,
+  collectCoverageFrom: ['src/**/*.ts'],
+  coveragePathIgnorePatterns: ['/templates/'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testPathIgnorePatterns: ['/__utils__/', '__fixtures__'],
+};
---
+++ packages/checkup-formatter-pretty/jest.setup.ts
@@ -0,0 +1,1 @@
+import '@microsoft/jest-sarif';
