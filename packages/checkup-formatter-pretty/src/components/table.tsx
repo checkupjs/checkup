ffebfa1341f8de9dc86178e69f85e930a4bdc705
Zhan Wang <zhanwang@zhanwang-mn1.linkedin.biz>
2021-08-04 00:38:11 -0700

    leverage ink-table to enhance table component

--- packages/checkup-formatter-pretty/src/components/table.tsx
+++ packages/checkup-formatter-pretty/src/components/table.tsx
@@ -1,10 +1,35 @@
 import React, { FC } from 'react';
-import { Box, Text } from 'ink';
+import { Box } from 'ink';
+import { default as InkTable } from 'ink-table';
 
-export const Table: FC<{ data: any }> = ({ data }) => {
+type Scalar = string | number | boolean | null | undefined;
+
+type ScalarDict = {
+  [key: string]: Scalar;
+};
+
+interface Result extends ScalarDict {
+  taskName: string;
+  uri: string;
+  message: string;
+  ruleId: string;
+}
+
+export const Table: FC<{ data: any[] }> = ({ data }) => {
+  const tableData = data.map((item) => {
+    const result: Result = {
+      taskName: item.properties.taskDisplayName,
+      uri: item.locations[0].physicalLocation.artifactLocation.uri,
+      message: item.message.text,
+      ruleId: item.ruleId,
+    };
+
+    return result;
+  });
+
   return (
     <Box>
-      <Text>I am a table {data}</Text>
+      <InkTable data={tableData} />
     </Box>
   );
 };
