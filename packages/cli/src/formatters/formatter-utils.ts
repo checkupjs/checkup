08244892358d6880402ffe23130a85bd7eb6928b
Steve Calvert <scalvert@linkedin.com>
2021-08-04 14:51:55 -0700

    Cleanup of garbage

--- packages/cli/src/formatters/formatter-utils.ts
+++ packages/cli/src/formatters/formatter-utils.ts
@@ -1,63 +1,59 @@
-// import { Notification } from 'sarif';
-// import { CheckupMetadata, FormatterArgs } from '@checkup/core';
-// import { yellow } from 'chalk';
+import { Notification } from 'sarif';
+import { CheckupMetadata, ConsoleWriter } from '@checkup/core';
+import { yellow } from 'chalk';
 
-// export function renderActions(actions: Notification[], args: FormatterArgs): void {
-//   if (actions && actions.length > 0) {
-//     args.writer.categoryHeader('Actions');
-//     actions.forEach((action: Notification) => {
-//       args.writer.log(`${yellow('■')} ${action.message.text}`);
-//     });
-//     args.writer.blankLine();
-//   }
-// }
+export function renderActions(actions: Notification[], writer: ConsoleWriter): void {
+  if (actions && actions.length > 0) {
+    writer.categoryHeader('Actions');
+    actions.forEach((action: Notification) => {
+      writer.log(`${yellow('■')} ${action.message.text}`);
+    });
+    writer.blankLine();
+  }
+}
 
-// export function renderInfo(metadata: CheckupMetadata, args: FormatterArgs) {
-//   let { analyzedFilesCount, project } = metadata;
-//   let { name, version, repository } = project;
+export function renderInfo(metadata: CheckupMetadata, writer: ConsoleWriter) {
+  let { analyzedFilesCount, project } = metadata;
+  let { name, version, repository } = project;
 
-//   let analyzedFilesMessage =
-//     repository.totalFiles !== analyzedFilesCount
-//       ? ` (${args.writer.emphasize(`${analyzedFilesCount} files`)} analyzed)`
-//       : '';
+  let analyzedFilesMessage =
+    repository.totalFiles !== analyzedFilesCount
+      ? ` (${writer.emphasize(`${analyzedFilesCount} files`)} analyzed)`
+      : '';
 
-//   args.writer.blankLine();
-//   args.writer.log(
-//     `Checkup report generated for ${args.writer.emphasize(
-//       `${name} v${version}`
-//     )}${analyzedFilesMessage}`
-//   );
-//   args.writer.blankLine();
-//   args.writer.log(
-//     `This project is ${args.writer.emphasize(
-//       `${repository.age} old`
-//     )}, with ${args.writer.emphasize(
-//       `${repository.activeDays} active days`
-//     )}, ${args.writer.emphasize(`${repository.totalCommits} commits`)} and ${args.writer.emphasize(
-//       `${repository.totalFiles} files`
-//     )}.`
-//   );
-//   args.writer.blankLine();
-// }
+  writer.blankLine();
+  writer.log(
+    `Checkup report generated for ${writer.emphasize(`${name} v${version}`)}${analyzedFilesMessage}`
+  );
+  writer.blankLine();
+  writer.log(
+    `This project is ${writer.emphasize(`${repository.age} old`)}, with ${writer.emphasize(
+      `${repository.activeDays} active days`
+    )}, ${writer.emphasize(`${repository.totalCommits} commits`)} and ${writer.emphasize(
+      `${repository.totalFiles} files`
+    )}.`
+  );
+  writer.blankLine();
+}
 
-// export function renderLinesOfCode(info: CheckupMetadata, args: FormatterArgs) {
-//   let { repository } = info.project;
+export function renderLinesOfCode(info: CheckupMetadata, writer: ConsoleWriter) {
+  let { repository } = info.project;
 
-//   args.writer.sectionedBar(
-//     repository.linesOfCode.types.map((type) => {
-//       return { title: type.extension, count: type.total };
-//     }),
-//     repository.linesOfCode.total,
-//     'lines of code'
-//   );
+  writer.sectionedBar(
+    repository.linesOfCode.types.map((type) => {
+      return { title: type.extension, count: type.total };
+    }),
+    repository.linesOfCode.total,
+    'lines of code'
+  );
 
-//   args.writer.blankLine();
-// }
+  writer.blankLine();
+}
 
-// export function renderCLIInfo(metadata: CheckupMetadata, args: FormatterArgs) {
-//   let { version, configHash } = metadata.cli;
+export function renderCLIInfo(metadata: CheckupMetadata, writer: ConsoleWriter) {
+  let { version, configHash } = metadata.cli;
 
-//   args.writer.dimmed(`checkup v${version}`);
-//   args.writer.dimmed(`config ${configHash}`);
-//   args.writer.blankLine();
-// }
+  writer.dimmed(`checkup v${version}`);
+  writer.dimmed(`config ${configHash}`);
+  writer.blankLine();
+}
--- packages/cli/src/formatters/pretty.ts
+++ packages/cli/src/formatters/pretty.ts
@@ -1,134 +1,140 @@
-// import {
-//   CheckupError,
-//   getRegisteredTaskReporters,
-//   groupDataByField,
-//   reduceResults,
-//   renderEmptyResult,
-//   ErrorKind,
-//   FormatterArgs,
-// } from '@checkup/core';
-// import * as cleanStack from 'clean-stack';
-// import { startCase } from 'lodash';
-// import { Notification, Result } from 'sarif';
-// import { renderActions, renderCLIInfo, renderInfo, renderLinesOfCode } from './formatter-utils';
-// import { writeResultFile } from './file-writer';
+import {
+  CheckupError,
+  getRegisteredTaskReporters,
+  groupDataByField,
+  reduceResults,
+  renderEmptyResult,
+  ErrorKind,
+  FormatterArgs,
+  ConsoleWriter,
+  BufferedWriter,
+  BaseOutputWriter,
+} from '@checkup/core';
+import * as cleanStack from 'clean-stack';
+import { startCase } from 'lodash';
+import { Notification, Result } from 'sarif';
+import { renderActions, renderCLIInfo, renderInfo, renderLinesOfCode } from './formatter-utils';
+import { writeResultFile } from './file-writer';
 
-// export default class PrettyFormatter {
-//   args: FormatterArgs;
+export default class PrettyFormatter {
+  args: FormatterArgs;
+  writer: BaseOutputWriter;
 
-//   constructor(args: FormatterArgs) {
-//     this.args = args;
-//   }
+  constructor(args: FormatterArgs) {
+    this.args = args;
 
-//   format() {
-//     let run = this.args.logParser.run;
-//     let metaData = this.args.logParser.metaData;
+    this.writer = this.args.outputFile ? new BufferedWriter() : new ConsoleWriter();
+  }
 
-//     renderInfo(metaData, this.args);
-//     renderLinesOfCode(metaData, this.args);
+  format() {
+    let run = this.args.logParser.run;
+    let metaData = this.args.logParser.metaData;
 
-//     this.renderTaskResults(run.results);
-//     this.renderErrors(this.args.logParser.exceptions);
+    renderInfo(metaData, this.writer);
+    renderLinesOfCode(metaData, this.writer);
 
-//     renderActions(this.args.logParser.actions, this.args);
+    this.renderTaskResults(run.results);
+    this.renderErrors(this.args.logParser.exceptions);
 
-//     if (process.env.CHECKUP_TIMING === '1') {
-//       this.renderTimings(this.args.logParser.timings);
-//     }
+    renderActions(this.args.logParser.actions, this.writer);
 
-//     renderCLIInfo(metaData, this.args);
+    if (process.env.CHECKUP_TIMING === '1') {
+      this.renderTimings(this.args.logParser.timings);
+    }
 
-//     if (this.args.outputFile) {
-//       writeResultFile(this.args.writer.outputString, this.args.cwd, this.args.outputFile);
-//     }
-//   }
+    renderCLIInfo(metaData, this.writer);
 
-//   renderTaskResults(results: Result[] | undefined): void {
-//     let currentCategory = '';
+    if (this.args.outputFile) {
+      writeResultFile((<BufferedWriter>this.writer).buffer, this.args.cwd, this.args.outputFile);
+    }
+  }
 
-//     if (results) {
-//       let resultsByRule = this.args.logParser.resultsByRule;
+  renderTaskResults(results: Result[] | undefined): void {
+    let currentCategory = '';
 
-//       resultsByRule.forEach(({ rule, results }) => {
-//         let category = rule.properties?.category;
+    if (results) {
+      let resultsByRule = this.args.logParser.resultsByRule;
 
-//         if (category !== currentCategory) {
-//           this.args.writer.categoryHeader(startCase(category));
-//           currentCategory = category;
-//         }
+      resultsByRule.forEach(({ rule, results }) => {
+        let category = rule.properties?.category;
 
-//         let reporter = this.getTaskReporter(rule.id);
+        if (category !== currentCategory) {
+          this.writer.categoryHeader(startCase(category));
+          currentCategory = category;
+        }
 
-//         reporter(results, this.args);
-//       });
-//     }
-//     this.args.writer.blankLine();
-//   }
+        let reporter = this.getTaskReporter(rule.id);
 
-//   getTaskReporter(taskName: string) {
-//     let registeredTaskReporters = getRegisteredTaskReporters();
-//     let reporter = registeredTaskReporters.get(taskName) || this.getReportComponent.bind(this);
+        reporter(results, this.writer);
+      });
+    }
+    this.writer.blankLine();
+  }
 
-//     if (typeof reporter === 'undefined') {
-//       throw new CheckupError(ErrorKind.TaskConsoleReporterNotFound, { taskName });
-//     }
+  getTaskReporter(taskName: string) {
+    let registeredTaskReporters = getRegisteredTaskReporters();
+    let reporter = registeredTaskReporters.get(taskName) || this.getReportComponent.bind(this);
 
-//     return reporter;
-//   }
+    if (typeof reporter === 'undefined') {
+      throw new CheckupError(ErrorKind.TaskConsoleReporterNotFound, { taskName });
+    }
 
-//   getReportComponent(taskResults: Result[]) {
-//     const groupedTaskResults = reduceResults(groupDataByField(taskResults, 'message.text'));
+    return reporter;
+  }
 
-//     this.args.writer.section(groupedTaskResults[0].properties?.taskDisplayName, () => {
-//       const totalResults = groupedTaskResults.length;
+  getReportComponent(taskResults: Result[]) {
+    const groupedTaskResults = reduceResults(groupDataByField(taskResults, 'message.text'));
 
-//       if (totalResults > 1) {
-//         this.args.writer.log(`Total: ${totalResults}`);
-//       }
+    this.writer.section(groupedTaskResults[0].properties?.taskDisplayName, () => {
+      const totalResults = groupedTaskResults.length;
 
-//       groupedTaskResults.forEach((result) => {
-//         if (result.message.text === 'No results found') {
-//           renderEmptyResult(result);
-//         } else {
-//           this.args.writer.value({
-//             title: result.message.text || (result.ruleId as string),
-//             count: result?.occurrenceCount || Number.NaN,
-//           });
-//         }
-//       });
-//     });
-//   }
+      if (totalResults > 1) {
+        this.writer.log(`Total: ${totalResults}`);
+      }
 
-//   renderErrors(notifications: Notification[] | undefined) {
-//     if (notifications && notifications.length > 0) {
-//       this.args.writer.table(
-//         notifications.map((notification) => {
-//           return [
-//             notification.associatedRule?.id as string,
-//             cleanStack(notification.exception?.stack?.frames.join('\n') || ''),
-//           ];
-//         }),
-//         ['Task Name', 'Error']
-//       );
-//     }
-//   }
+      groupedTaskResults.forEach((result) => {
+        if (result.message.text === 'No results found') {
+          renderEmptyResult(result);
+        } else {
+          this.writer.value({
+            title: result.message.text || (result.ruleId as string),
+            count: result?.occurrenceCount || Number.NaN,
+          });
+        }
+      });
+    });
+  }
 
-//   renderTimings(timings: Record<string, number>) {
-//     let total = Object.values(timings).reduce((total, timing) => (total += timing), 0);
+  renderErrors(notifications: Notification[] | undefined) {
+    if (notifications && notifications.length > 0) {
+      this.writer.table(
+        notifications.map((notification) => {
+          return [
+            notification.associatedRule?.id as string,
+            cleanStack(notification.exception?.stack?.frames.join('\n') || ''),
+          ];
+        }),
+        ['Task Name', 'Error']
+      );
+    }
+  }
 
-//     this.args.writer.categoryHeader('Task Timings');
+  renderTimings(timings: Record<string, number>) {
+    let total = Object.values(timings).reduce((total, timing) => (total += timing), 0);
 
-//     this.args.writer.table(
-//       Object.keys(timings)
-//         .map((taskName) => [
-//           taskName,
-//           Number.parseFloat(timings[taskName].toFixed(2)),
-//           `${((timings[taskName] * 100) / total).toFixed(1)}%`,
-//         ])
-//         .sort((a, b) => (b[1] as number) - (a[1] as number)),
-//       ['Task Name', 'Time (sec)', 'Relative']
-//     );
+    this.writer.categoryHeader('Task Timings');
 
-//     this.args.writer.blankLine();
-//   }
-// }
+    this.writer.table(
+      Object.keys(timings)
+        .map((taskName) => [
+          taskName,
+          Number.parseFloat(timings[taskName].toFixed(2)),
+          `${((timings[taskName] * 100) / total).toFixed(1)}%`,
+        ])
+        .sort((a, b) => (b[1] as number) - (a[1] as number)),
+      ['Task Name', 'Time (sec)', 'Relative']
+    );
+
+    this.writer.blankLine();
+  }
+}
