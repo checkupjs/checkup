d1bd656cea2ba4fb498a5416674fa7f8c17875f1
Steve Calvert <scalvert@linkedin.com>
2021-08-18 14:35:42 -0700

    Removing pretty from get-formatter (it should be used as an external formatter)

--- packages/cli/src/formatters/get-formatter.ts
+++ packages/cli/src/formatters/get-formatter.ts
@@ -10,7 +10,6 @@ import {
 } from '@checkup/core';
 import { Log } from 'sarif';
 // eslint-disable-next-line node/no-extraneous-import
-import { PrettyFormatter } from 'checkup-formatter-pretty';
 import SummaryFormatter from './summary';
 import JsonFormatter from './json';
 
@@ -27,11 +26,6 @@ export function getFormatter(options: FormatterOptions) {
   switch (mergedOptions.format) {
     case OutputFormat.summary: {
       Formatter = SummaryFormatter;
-      break;
-    }
-
-    case OutputFormat.pretty: {
-      Formatter = PrettyFormatter;
       break;
     }
 
