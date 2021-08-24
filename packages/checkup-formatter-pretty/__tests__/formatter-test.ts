1d3337875ff72e97b1db2049522c4188dfc6752d
Zhan Wang <zhanwang@zhanwang-mn1.linkedin.biz>
2021-08-23 23:05:23 -0700

    use ink test library render function directly

--- packages/checkup-formatter-pretty/__tests__/formatter-test.ts
+++ packages/checkup-formatter-pretty/__tests__/formatter-test.ts
@@ -1,4 +1,3 @@
-/* eslint-disable jest/no-disabled-tests */
 import { resolve } from 'path';
 import { readJsonSync } from 'fs-extra';
 import { CheckupLogParser } from '@checkup/core';
@@ -7,45 +6,7 @@ import { Options } from '../src/types';
 const stripAnsi = require('strip-ansi');
 
 describe('Test Pretty formatter', () => {
-  it('use ink test library directly', () => {
-    const log = readJsonSync(resolve(__dirname, './__fixtures__/checkup-result.sarif'));
-    const logParser = new CheckupLogParser(log);
-    const options: Options = {
-      cwd: '',
-      format: 'checkup-formatter-pretty',
-    };
-
-    let formatter = new PrettyFormatter(options);
-    const result = formatter.testRender(logParser);
-
-    expect(stripAnsi(result)).toMatchInlineSnapshot(`
-      "Checkup report generated for travis v0.0.1  (1797 files analyzed)
-      This project is 9 years old, with 1448 active days, 5983 commits and 1667 files
-
-
-      lines of code 101513
-      ■■■■■■■■■■■■■■■■■■■■■■■■■ js (49161)
-      ■■■■■■■■■■■■ svg (24112)
-      ■■■■■■■■ scss (14936)
-      ■■■■■■■ hbs (12464)
-      ■ rb (639)
-      ■ html (201)
-
-
-      === metrics
-      ┌─────────────┬───────────────┐
-      │ ruleId      │ result(value) │
-      ├─────────────┼───────────────┤
-      │ ember-types │ 810           │
-      └─────────────┴───────────────┘
-
-
-      checkup v1.0.0-beta.11
-      config 7bca477eada135bcfae0876e271fff89"
-    `);
-  });
-
-  it.skip('can generate string from format', () => {
+  it('can generate string from format', () => {
     const log = readJsonSync(resolve(__dirname, './__fixtures__/checkup-result.sarif'));
     const logParser = new CheckupLogParser(log);
     const options: Options = {
@@ -80,8 +41,7 @@ describe('Test Pretty formatter', () => {
 
 
       checkup v1.0.0-beta.11
-      config 7bca477eada135bcfae0876e271fff89
-      "
+      config 7bca477eada135bcfae0876e271fff89"
     `);
   });
 });
--- packages/checkup-formatter-pretty/src/index.ts
+++ packages/checkup-formatter-pretty/src/index.ts
@@ -1,41 +1,40 @@
-import { EventEmitter } from 'events';
+// import { EventEmitter } from 'events';
 import * as React from 'react';
-import { render as inkRender } from 'ink';
-import { Instance as InkInstance } from 'ink';
-import { ReactElement } from 'react';
+// import { render as inkRender } from 'ink';
+// import { Instance as InkInstance } from 'ink';
+// import { ReactElement } from 'react';
 import { render as inkTestRender } from 'ink-testing-library';
 import { CheckupLogParser, Formatter } from '@checkup/core';
 import { default as pretty } from './pretty-formatter';
 import { Options } from './types';
-const console = require('console');
 
-interface Instance {
-  stdout: Stdout;
-}
+// interface Instance {
+//   stdout: Stdout;
+// }
 
-class Stdout extends EventEmitter {
-  private _lastFrame?: string;
+// class Stdout extends EventEmitter {
+//   private _lastFrame?: string;
 
-  write = (frame: string) => {
-    this._lastFrame = frame;
-  };
+//   write = (frame: string) => {
+//     this._lastFrame = frame;
+//   };
 
-  lastFrame = () => {
-    return this._lastFrame;
-  };
-}
+//   lastFrame = () => {
+//     return this._lastFrame;
+//   };
+// }
 
-const instances: InkInstance[] = [];
+// const instances: InkInstance[] = [];
 
-const render = (tree: ReactElement): Instance => {
-  const stdout = new Stdout();
-  const instance = inkRender(tree, { stdout: stdout as any });
-  // console.log('In render function: instance:', instance);
-  // console.log('In render function: stdout:', stdout);
-  instances.push(instance);
+// const render = (tree: ReactElement): Instance => {
+//   const stdout = new Stdout();
+//   const instance = inkRender(tree, { stdout: stdout as any });
+//   // console.log('In render function: instance:', instance);
+//   // console.log('In render function: stdout:', stdout);
+//   instances.push(instance);
 
-  return { stdout };
-};
+//   return { stdout };
+// };
 
 class PrettyFormatter implements Formatter {
   options: Options;
@@ -44,16 +43,11 @@ class PrettyFormatter implements Formatter {
     this.options = options;
   }
 
-  format(logParser: CheckupLogParser) {
-    const { stdout } = render(React.createElement(pretty, { logParser }));
-    // console.log('After render:', stdout);
-    return stdout.lastFrame();
-  }
-
-  testRender(logParser: CheckupLogParser) {
+  format(logParser: CheckupLogParser): string {
     const { lastFrame } = inkTestRender(React.createElement(pretty, { logParser }));
     const result = lastFrame();
-    return result;
+
+    return result || 'No output to checkup-formatter-pretty';
   }
 }
 
