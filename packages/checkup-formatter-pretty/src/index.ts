bde4fef2f774125583199315a45aeb93e38d65c4
Zhan Wang <zhanwang@zhanwang-mn1.linkedin.biz>
2021-08-24 22:09:58 -0700

    upgrade ink-render-string version

--- packages/checkup-formatter-pretty/src/index.ts
+++ packages/checkup-formatter-pretty/src/index.ts
@@ -1,38 +1,8 @@
-import { EventEmitter } from 'events';
 import * as React from 'react';
-// import { render } from 'ink-render-string';
+import { render } from 'ink-render-string';
 import { CheckupLogParser, Formatter } from '@checkup/core';
-import { render as inkRender } from 'ink';
-import { ReactElement } from 'react';
 import { Options } from './types';
 import { default as pretty } from './pretty-formatter';
-
-class Stdout extends EventEmitter {
-  readonly frames: string[] = [];
-  private _lastFrame?: string;
-
-  write = (frame: string) => {
-    this.frames.push(frame);
-    this._lastFrame = frame;
-  };
-
-  lastFrame = () => {
-    return this._lastFrame;
-  };
-}
-
-export const render = (tree: ReactElement): string => {
-  const stdout = new Stdout();
-
-  inkRender(tree, {
-    stdout: stdout as any,
-    // debug: true,
-    // exitOnCtrlC: false,
-    // patchConsole: false,
-  });
-
-  return stdout.lastFrame() || 'stdout.lastFrame() is undefined';
-};
 
 class PrettyFormatter implements Formatter {
   options: Options;
