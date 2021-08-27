4486441a323bdb46df23b614b4e136a40c58499d
Steve Calvert <scalvert@linkedin.com>
2021-08-27 15:31:23 -0700

    Narrowing type of map value for registering components

--- packages/checkup-formatter-pretty/src/component-provider.ts
+++ packages/checkup-formatter-pretty/src/component-provider.ts
@@ -1,9 +1,10 @@
 import { readdirSync } from 'fs';
 import { join, parse } from 'path';
+import * as React from 'react';
 
-export const registeredComponents = new Map();
+export const registeredComponents = new Map<string, React.FC>();
 
-export function registerDefaultComponents(): Record<string, any> {
+export function registerDefaultComponents(): Map<string, React.FC> {
   let builtInComponents = new Set(
     readdirSync(join(__dirname, 'components')).map((file) => {
       return parse(file).base.split('.')[0];
@@ -13,7 +14,7 @@ export function registerDefaultComponents(): Record<string, any> {
   for (let component of builtInComponents) {
     registeredComponents.set(
       component,
-      Object.values(require(join(__dirname, 'components', component))).pop()
+      Object.values(require(join(__dirname, 'components', component))).pop() as React.FC
     );
   }
 
