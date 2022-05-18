e7ec696688d76979c335bb49188488987794705f
Steve Calvert <scalvert@linkedin.com>
2022-05-17 17:34:42 -0700

    Fixig tests

--- packages/checkup-formatter-pretty/vitest.config.ts
+++ packages/checkup-formatter-pretty/vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   test: {
-    include: ['__tests__/**/*-test.ts'],
-    global: true,
+    include: ['__tests__/**/*-test.{ts,tsx}'],
+    globals: true,
   },
 });
