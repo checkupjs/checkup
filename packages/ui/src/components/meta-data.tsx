f72ca4d0400bcb6fdd7d69fa9849194c59f0705d
Steve Calvert <scalvert@linkedin.com>
2022-05-31 12:02:33 -0700

    Moving components out for reuse

---
+++ packages/ui/src/components/meta-data.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { CheckupMetadata } from '@checkup/core';
+import { Box, Text } from 'ink';
+
+export const MetaData: React.FC<{ metaData: CheckupMetadata }> = ({ metaData }) => {
+  let { analyzedFilesCount, project } = metaData;
+  let { name, version, repository } = project;
+  let analyzedFilesMessage =
+    repository.totalFiles !== analyzedFilesCount ? ` (${analyzedFilesCount} files analyzed)` : '';
+
+  return (
+    <>
+      <Box flexDirection="column" marginBottom={1}>
+        <Box marginBottom={1}>
+          <Text>
+            Checkup report generated for {name} v{version} {analyzedFilesMessage}
+          </Text>
+        </Box>
+        <Box marginBottom={1}>
+          <Text>
+            This project is {repository.age} old, with {repository.activeDays} active days,{' '}
+            {repository.totalCommits} commits and {repository.totalFiles} files
+          </Text>
+        </Box>
+      </Box>
+    </>
+  );
+};
--- packages/ui/src/formatters/pretty.tsx
+++ packages/ui/src/formatters/pretty.tsx
@@ -3,6 +3,7 @@ import { Box, Text, Newline } from 'ink';
 import { CheckupLogParser, CheckupMetadata, TaskName, RuleResults } from '@checkup/core';
 import { ReportingDescriptor } from 'sarif';
 import { default as InkTable } from 'ink-table';
+import { MetaData } from '../components/meta-data.js';
 import { registeredComponents } from '../component-provider.js';
 
 const PrettyFormatter: React.FC<{ logParser: CheckupLogParser }> = ({ logParser }) => {
@@ -17,31 +18,6 @@ const PrettyFormatter: React.FC<{ logParser: CheckupLogParser }> = ({ logParser 
       <RenderTiming timings={logParser.timings} />
       <CLIInfo metaData={metaData} />
     </Box>
-  );
-};
-
-const MetaData: React.FC<{ metaData: CheckupMetadata }> = ({ metaData }) => {
-  let { analyzedFilesCount, project } = metaData;
-  let { name, version, repository } = project;
-  let analyzedFilesMessage =
-    repository.totalFiles !== analyzedFilesCount ? ` (${analyzedFilesCount} files analyzed)` : '';
-
-  return (
-    <>
-      <Box flexDirection="column" marginBottom={1}>
-        <Box marginBottom={1}>
-          <Text>
-            Checkup report generated for {name} v{version} {analyzedFilesMessage}
-          </Text>
-        </Box>
-        <Box marginBottom={1}>
-          <Text>
-            This project is {repository.age} old, with {repository.activeDays} active days,{' '}
-            {repository.totalCommits} commits and {repository.totalFiles} files
-          </Text>
-        </Box>
-      </Box>
-    </>
   );
 };
 
