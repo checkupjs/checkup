9b3d333b76bc583ec21e8cb3573f84b989c8e5f9
Steve Calvert <scalvert@linkedin.com>
2022-06-30 14:30:23 -0700

    Renaming test directories (#1249)

--- packages/cli/vitest.config.ts
+++ packages/cli/vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   test: {
-    include: ['__tests__/**/*-test.{ts,tsx}'],
+    include: ['tests/**/*-test.{ts,tsx}'],
     globals: true,
     testTimeout: 200_000,
   },
--- packages/test-helpers/vitest.config.ts
+++ packages/test-helpers/vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   test: {
-    include: ['__tests__/**/*-test.ts'],
+    include: ['tests/**/*-test.ts'],
     global: true,
   },
 });
